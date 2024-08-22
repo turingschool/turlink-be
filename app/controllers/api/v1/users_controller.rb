8c03390bc05d08df60a7ef326455385a162bc3bd
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-22 16:15:02 -0600

    feat: sets a session id in the user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       render json: UserSerializer.new(user), status: :created
     else
       render json: ErrorSerializer.new(user, :unprocessable_entity).serialize, status: :unprocessable_entity
