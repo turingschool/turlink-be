cdecc299ce9ea29aed2eaf93ab8d57320143127f
James McHugh <jdmchugh111@gmail.com>
2024-08-22 13:45:08 -0400

    Created model associations and validations for User and Link - fully tested, also added pull request template

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Link, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:original)}
+    it {should validate_presence_of(:short)}
+    it 'should validate presence of user_id' do
+      user = User.create(email: "james@example.com", password: "james123")
+      link = Link.new(original: "www.example.com", short: "tur.link/12345678", user_id: user.id)
+      expect(link).to be_valid
+    end
+    it {should validate_presence_of(:user_id)}
+  end
+
+  describe "relationships" do
+    it { should belong_to :user }
+  end
+end
