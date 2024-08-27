c807eb709a97cdb00ece06bc79ae1032d59c559f
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-27 10:50:16 -0600

    feat: testing the click count on the show action

--- spec/requests/api/v1/links_request_spec.rb
+++ spec/requests/api/v1/links_request_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'links requests', type: :request do
   describe 'POST /users/:id/links' do
     it 'creates a new Link record with a shortened link' do
-      user1 = User.create(email: "user@example.com", password: "user123")
-      
+      user1 = User.create(email: 'user@example.com', password: 'user123')
+
       post "/api/v1/users/#{user1.id}/links?link=long-link-example.com"
 
       expect(response).to be_successful
@@ -12,19 +12,19 @@ RSpec.describe 'links requests', type: :request do
 
       link = JSON.parse(response.body, symbolize_names: true)[:data]
 
-      expect(link).to be_a Hash 
+      expect(link).to be_a Hash
       expect(link).to have_key :id
-      expect(link[:type]).to eq("link")
+      expect(link[:type]).to eq('link')
       attributes = link[:attributes]
-      expect(attributes[:original]).to eq("long-link-example.com")
+      expect(attributes[:original]).to eq('long-link-example.com')
       expect(attributes[:user_id]).to eq(user1.id)
       expect(attributes[:short]).to be_a String
     end
 
     describe 'sad path' do
-      it "will return 422 if link param is not entered" do
-        user1 = User.create(email: "user@example.com", password: "user123")
-      
+      it 'will return 422 if link param is not entered' do
+        user1 = User.create(email: 'user@example.com', password: 'user123')
+
         post "/api/v1/users/#{user1.id}/links"
 
         expect(response).to_not be_successful
@@ -32,28 +32,28 @@ RSpec.describe 'links requests', type: :request do
 
         error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-        expect(error[:status]).to eq("unprocessable_entity")
+        expect(error[:status]).to eq('unprocessable_entity')
         expect(error[:message]).to eq("Original can't be blank")
       end
 
-      it "will return 404 if user_id does not exist" do
-        post "/api/v1/users/99999999/links?link=this-wont-work.com"
+      it 'will return 404 if user_id does not exist' do
+        post '/api/v1/users/99999999/links?link=this-wont-work.com'
 
         expect(response).to_not be_successful
         expect(response.status).to eq(422)
 
         error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-        expect(error[:status]).to eq("unprocessable_entity")
-        expect(error[:message]).to eq("User must exist")
+        expect(error[:status]).to eq('unprocessable_entity')
+        expect(error[:message]).to eq('User must exist')
       end
     end
   end
 
   describe 'GET /users/:id/links' do
-    it "retrieves the user object with all associated links as attributes" do
-      user1 = User.create(email: "user@example.com", password: "user123")
-      
+    it 'retrieves the user object with all associated links as attributes' do
+      user1 = User.create(email: 'user@example.com', password: 'user123')
+
       post "/api/v1/users/#{user1.id}/links?link=long-link-example.com"
       post "/api/v1/users/#{user1.id}/links?link=another-long-link.com"
 
@@ -65,35 +65,35 @@ RSpec.describe 'links requests', type: :request do
       user = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(user).to have_key :id
-      expect(user[:type]).to eq("user")
-      expect(user[:attributes][:email]).to eq("user@example.com")
+      expect(user[:type]).to eq('user')
+      expect(user[:attributes][:email]).to eq('user@example.com')
       links = user[:attributes][:links]
       expect(links.count).to eq(2)
       first_link = links[0]
       expect(first_link).to have_key :id
-      expect(first_link[:original]).to eq("long-link-example.com")
+      expect(first_link[:original]).to eq('long-link-example.com')
       expect(first_link[:short]).to be_a String
       expect(first_link[:user_id]).to eq(user1.id)
     end
 
-    describe "sad path" do
-      it "will return 404 if user does not exist" do
-        get "/api/v1/users/999999/links"
+    describe 'sad path' do
+      it 'will return 404 if user does not exist' do
+        get '/api/v1/users/999999/links'
 
         expect(response).to_not be_successful
         expect(response.status).to eq(404)
 
         error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-        expect(error[:message]).to eq("Record not found")
+        expect(error[:message]).to eq('Record not found')
       end
     end
   end
 
   describe 'GET /links' do
-    it "can return a the json for a link (including full url) when given shortened link" do
-      user1 = User.create(email: "user@example.com", password: "user123")
-      
+    it 'can return a the json for a link (including full url) when given shortened link' do
+      user1 = User.create(email: 'user@example.com', password: 'user123')
+
       post "/api/v1/users/#{user1.id}/links?link=long-link-example.com"
 
       link = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -108,22 +108,60 @@ RSpec.describe 'links requests', type: :request do
       link2 = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(link2).to have_key :id
-      expect(link2[:type]).to eq("link")
-      expect(link2[:attributes][:original]).to eq("long-link-example.com")
+      expect(link2[:type]).to eq('link')
+      expect(link2[:attributes][:original]).to eq('long-link-example.com')
       expect(link2[:attributes][:short]).to eq(short)
       expect(link2[:attributes][:user_id]).to eq(user1.id)
     end
 
-    describe "sad path" do
+    it 'returns a link with click_count and last_click attributes' do
+      user1 = User.create(email: 'user@example.com', password: 'user123')
+
+      post "/api/v1/users/#{user1.id}/links?link=long-link-example.com"
+
+      link = JSON.parse(response.body, symbolize_names: true)[:data]
+      short = link[:attributes][:short]
+
+      get "/api/v1/links?short=#{short}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      link2 = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(link2[:attributes]).to have_key :click_count
+      expect(link2[:attributes]).to have_key :last_click
+      expect(link2[:attributes][:click_count]).to eq(1)
+      expect(link2[:attributes][:last_click]).to be_present
+    end
+
+    it 'can update click count multiple times' do
+      user1 = User.create(email: 'user@example.com', password: 'user123')
+
+      post "/api/v1/users/#{user1.id}/links?link=long-link-example.com"
+
+      link = JSON.parse(response.body, symbolize_names: true)[:data]
+      short = link[:attributes][:short]
+
+      get "/api/v1/links?short=#{short}"
+      get "/api/v1/links?short=#{short}"
+      get "/api/v1/links?short=#{short}"
+
+      link2 = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(link2[:attributes][:click_count]).to eq(3)
+    end
+
+    describe 'sad path' do
       it "will return a 404 if shortened link doesn't exist" do
-        get "/api/v1/links?short=tur.link/12345678"
+        get '/api/v1/links?short=tur.link/12345678'
 
         expect(response).to_not be_successful
         expect(response.status).to eq(404)
 
         error = JSON.parse(response.body, symbolize_names: true)[:errors][0]
 
-        expect(error[:message]).to eq("Record not found")
+        expect(error[:message]).to eq('Record not found')
       end
     end
   end
